6fd828ad566145883414d33fe1d6380c6dbb6bcf
Olushola Michael <olusholamichael25@gmail.com>
2023-05-25 12:43:28 +0000

    This is an update on the previous edition using one line code,list comprehension

--- Olushola.py
+++ Olushola.py
@@ -1,10 +1,11 @@
-'''This is my first python file deployment'''
-even=[]
-for i in range(100):
-    if i % 2 == 0:
-        even.append(i)
-print(even)
+# '''This is my first python file deployment'''
+# even=[]
+# for i in range(100):
+#     if i % 2 == 0:
+#         even.append(i)
+# print(even)
 
-
+evens = [x for x in range(100) if x % 2 == 0]
+print(evens)
 
 
